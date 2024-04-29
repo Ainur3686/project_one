86a52181802803c3b82592e6969b96850d7621c6
Ainur Botagarova <botagarovaainura@gmail.com>
2024-04-29 13:40:50 -0500

    Added Step 2 with the 'move' function

--- OneD_tictactoe.py
+++ OneD_tictactoe.py
@@ -1,4 +1,4 @@
-#Step1. Write a function evaluate that accepts the string with the board as argument, returns one character 
+#Step1. Write a function 'evaluate' that accepts the string with the board as argument, returns one character 
 def evaluate(board):
     #Returns the character based on the status of the game
     if "xxx" in board:
@@ -9,3 +9,14 @@ def evaluate(board):
         return "!"
     else:
         return "-"
+
+#Step2. Write a 'move' function (accepts the string with the board, a position (0-19) and mark(x or o), returns a board)
+def move(board, mark, position):
+    #Returns the game board with the given mark in the given position
+    #splicing the substring from 'start position' up to the 'position'
+    #concatinating the 'mark'
+    #concatinating the rest of the board, excluding the position of the 'mark'
+    #updating the board
+    board = board[0:position]+ mark + board[position + 1:]
+    #returning the updated board
+    return board
